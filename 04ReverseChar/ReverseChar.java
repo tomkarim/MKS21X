3424048153371d64632e1825f9395f74d6cae45c
Tom Karim <tomkarim6@gmail.com>
2017-11-22 14:38:57 -0500

    v0.5 of ReverseChar

---
+++ 04ReverseChar/ReverseChar.java
@@ -0,0 +1,20 @@
+public class ReverseChar implements CharSequence{
+    private ReverseChar ogstring;
+
+    public ReverseChar(CharSequence ogstring) {
+	this.ogstring = ogstring;
+	    }
+
+    public int length(){
+	return ogstring.length();
+	    }
+
+    public char charAt(int index){
+	return charAt(index);
+    }
+
+    public CharSequence subSequence(int start, int end){
+	return ogstring.substring(start, end);
+    }
+
+}
