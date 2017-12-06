d86c0b1b74b61134c3475525228beb93bd029eaf
Tom Karim <tomkarim6@gmail.com>
2017-12-06 06:12:19 -0800

    creating array for toCode

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -1,5 +1,6 @@
 public class Barcode implements Comparable<Barcode>{
     private String zip;
+    String[] codes = {"||:::",}
 
     public Barcode(String zip){
         if(zip.length() != 5){
@@ -14,11 +15,18 @@ public class Barcode implements Comparable<Barcode>{
     return   }
 
     public int compareTo(Barcode other){
-
+      return Integer.parseInt(zip) - Integer.parseInt(other.zip);
     }
 
     public static String toCode(String zip){
-
+      if(zip.length() != 5){
+          throw new IllegalArgumentException("Code must only be 5 characaters long!");
+      }
+      if(!isInteger(zip)){
+          throw new IllegalArgumentException("Code should only contain digits!");
+      }
+      String complete = zip +checkSum(zip);
+      int len = complete.length();
 
     }
 
