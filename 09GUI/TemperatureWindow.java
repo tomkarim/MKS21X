0c80b291b34f188f565c261e2ddfcfa99082f49f
Tom Karim <tomkarim6@gmail.com>
2017-12-13 14:42:53 -0500

    Made the pane, it doesnt do anything yet! Has a couple nonfunctioning buttons.

--- 09GUI/TemperatureWindow.java
+++ 09GUI/TemperatureWindow.java
@@ -2,3 +2,24 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+public class TemperatureWindow extends JFrame implements ActionListener{
+    private Container pane;
+    private JButton cf;
+    private Jbutton fc;
+    private JLabel C;
+    private JLabel F;
+    private JTextField c;
+    private JTextField f;
+
+    public TemperatureWindow(){
+	this.setTitle("Fahrenheit/Celcius Temperature Converter");
+	this.setSize(1000,800);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	pane = this.getContentPane();
+	pane.setLayoyt(new FlowLayout());
+
+	cf = new JButton("Make it Fahrenheit!");
+	fc = new JButton("Make it Celcius!");
+	
