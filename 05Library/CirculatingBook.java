4cb0dbb41a3116616efc3ab132c92d1f2b70ab40
T <T@Akbars-iMac.local>
2018-01-07 22:57:58 -0500

    filled in blanks for CirculatingBook

---
+++ 05Library/CirculatingBook.java
@@ -0,0 +1,56 @@
+public class CirculatingBook extends LibraryBook {
+    private String currentHolder, dueDate;
+
+    public CirculatingBook(String author, String title, String ISBN, String callNumber) {
+        super(author, title, ISBN, callNumber);
+        returned();
+    }
+
+    public String getCurrentHolder() {
+        return currentHolder;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setCurrentHolder(String currentHolder) {
+        this.currentHolder = currentHolder;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void checkout(String patron, String due) {
+        currentHolder = patron;
+        dueDate = due;
+    }
+
+    public void returned() {
+        currentHolder = "";
+        dueDate = "";
+    }
+
+    public String circulationStatus() {
+        if (currentHolder.equals("")) {
+            return "Book available on shelves";
+        }
+        return "book checked out by: " + currentHolder + " until: " + dueDate;
+
+    }
+
+    public String toString() {
+        if (currentHolder.equals("")) {
+            return super.toString();
+        }
+        return super.toString() + circulationStatus();
+    }
+}
+
+
+
+
+
+
+}
